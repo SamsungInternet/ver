f657df02fd89faa878a3d85183e64cc972340e8a
diekus <diekus@gmail.com>
2018-04-03 19:33:12 +0100

    adds initial flickr images downloader

---
+++ js/flickrImporter.js
@@ -0,0 +1,25 @@
+const key = '463053799829763cff950c7b3096b5c0';
+let flickr_url = 'https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=' + key + '&format=json&nojsoncallback=1';
+let img_src = null;
+
+function reqListener(){
+    img_src = JSON.parse(this.responseText);
+}
+
+let oReq = new XMLHttpRequest();
+oReq.addEventListener('load', reqListener);
+oReq.open('GET', flickr_url);
+oReq.send();
+
+
+
+function createImages(cant){
+    let img_urls = [cant];
+    for(i = 0; i < cant; i++){
+        let picUrl = picNum => `https://farm${ img_src.photos.photo[picNum].farm }.staticflickr.com/${ img_src.photos.photo[picNum].server }/${ img_src.photos.photo[picNum].id}_${ img_src.photos.photo[picNum].secret }.jpg;`;
+        let img = document.createElement('a-image');
+        img.setAttribute('src', picUrl(i));
+        document.querySelector('a-scene').appendChild(img);
+    }
+}
+
