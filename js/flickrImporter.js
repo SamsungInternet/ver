2994c3b3ffb1744fe14ef22427342752483c679f
diekus <diekus@gmail.com>
2018-04-05 21:34:31 +0100

    initial screen

--- js/flickrImporter.js
+++ js/flickrImporter.js
@@ -5,9 +5,9 @@ let img_w = 4.5;
 let img_h = 3.5;
 let spacer = 0.3;
 const num_imgs = 50;
-let circ_rad = 7;
+let circ_rad = 4;
 const trans_lapse = 500;
-
+const dur_vuelta = 840000;
 document.addEventListener('DOMContentLoaded', function() {    
     //creates the layout entity once the scene is loaded
     document.querySelector('a-scene').addEventListener('loaded', function() {
@@ -23,6 +23,7 @@ function init(){
     oReq.addEventListener('load', reqListener);
     oReq.open('GET', flickr_url);
     oReq.send();
+
 }
 
 /* flickr image setup */
@@ -30,6 +31,10 @@ function reqListener(){
     img_src = JSON.parse(this.responseText);
     console.log(img_src);
     createImages(num_imgs);
+    setCircleLayout(4);
+    let layout = document.getElementById('layout_container')
+    layout.object3D.rotateY(toRadians(180));
+    createUI();
 }
 
 function createImages(cant){
@@ -42,6 +47,40 @@ function createImages(cant){
         img.setAttribute('position', `${-10 + img_w * i } 2 -5`);
         document.querySelector('#layout_container').appendChild(img);
     }
+}
+
+/* UI setup */
+function createUI(){
+    let init_ui = document.createElement('a-entity');
+    init_ui.setAttribute('id', 'main_menu');
+    
+    let bg_pnl = document.createElement('a-image');
+    bg_pnl.setAttribute('src', '#bg_menu');
+    bg_pnl.setAttribute('transparent', 'true');
+    bg_pnl.setAttribute('height', 4);
+    bg_pnl.setAttribute('width', 2.2);
+    bg_pnl.setAttribute('position', '0 1.5 -3');
+
+    let ins = document.createElement('a-image');
+    ins.setAttribute('src', '#ins_gear');
+    ins.setAttribute('transparent', 'true');
+    ins.setAttribute('height', 1.5);
+    ins.setAttribute('width', 1.5);
+    ins.setAttribute('position', '0 1.2 -2.8');
+    
+    init_ui.appendChild(bg_pnl);
+    init_ui.appendChild(ins);
+
+    document.querySelector('a-scene').appendChild(init_ui);
+    rotateLayout();
+} 
+
+function rotateLayout(){
+    let layout = document.getElementById('layout_container');
+    
+    let tween_rot = new AFRAME.TWEEN.Tween(layout.object3D.rotation).to({y:toRadians(359)}, dur_vuelta);
+    tween_rot.repeat(Infinity);
+    tween_rot.start();
 }
 
 /* image layouts */
@@ -59,9 +98,11 @@ function setCircleLayout(r){
     for(i = 0; i < num_imgs; i++){
         //rotation: imgs[i].object3D.rotateY(toRadians(180-(90-ang * -i)));
         let tween_rot = new AFRAME.TWEEN.Tween(imgs[i].object3D.rotation).to({y:toRadians(180-(90-ang * -i))}, trans_lapse);
+        tween_rot.easing(TWEEN.Easing.Cubic.Out);
         tween_rot.start();
         //position: imgs[i].object3D.position.set(circ_rad * Math.cos(toRadians(ang * i)), 2, circ_rad * Math.sin(toRadians(ang * i)));
-        let tween_pos = new AFRAME.TWEEN.Tween(imgs[i].object3D.position).to({x:(circ_rad * Math.cos(toRadians(ang * i))), y: 2, z:(circ_rad * Math.sin(toRadians(ang * i)))}, trans_lapse);
+        let tween_pos = new AFRAME.TWEEN.Tween(imgs[i].object3D.position).to({x:(circ_rad * Math.cos(toRadians(ang * i))), y: 1, z:(circ_rad * Math.sin(toRadians(ang * i)))}, trans_lapse);
+        tween_pos.easing(TWEEN.Easing.Cubic.Out);
         tween_pos.start();
     }
 }
@@ -72,6 +113,7 @@ function setGridLayout(pc){
     for(i = 0; i < num_imgs; i++){
         //position: imgs[i].object3D.position.set(c * img_w + spacer, f * img_h, -6);
         let tween_pos = new AFRAME.TWEEN.Tween(imgs[i].object3D.position).to({x:(c * img_w + spacer), y: (f * img_h), z:-6}, trans_lapse);
+        tween_pos.easing(TWEEN.Easing.Cubic.Out);
         tween_pos.start();
         imgs[i].object3D.rotation.set(0, 0, 0);
         c++;
